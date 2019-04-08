1a98cd803e5b84741c119846442cd0af720bc1cc
izumiisme <bmbm.taeha@gmail.com>
2019-04-08 21:25:39 +0900

    4/8

--- login/sql/struts.sql
+++ login/sql/struts.sql
@@ -4,13 +4,13 @@ drop database if exists logindb_fujie;
 create database logindb_fujie;
 use logindb_fujie;
 
-create table user(
+create table user_fujie(
 id int,
 user_name varchar(255),
 password varchar(255)
 );
 
-insert into user values
+insert into user_fujie values
 (1,"taro","123"),
 (2,"jiro","123"),
 (3,"hanako","123"),
--- login/src/com/internousdev/login/dao/LoginDAO.java
+++ login/src/com/internousdev/login/dao/LoginDAO.java
@@ -13,7 +13,7 @@ public class LoginDAO {
 		DBConnector db=new DBConnector();
 		Connection con=db.getConnection();
 
-		String sql ="select*from user where user_name=? and password=?";
+		String sql ="select*from user_fujie where user_name=? and password=?";
 
 	try{
 		PreparedStatement ps=con.prepareStatement(sql);
