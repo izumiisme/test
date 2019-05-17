02b443926f8722948d84c1a2dc2efb859464ab23
izumiisme <bmbm.taeha@gmail.com>
2019-05-17 20:55:22 +0900

    5/17

--- login4/src/com/internousdev/login/util/DBConnector.java
+++ login4/src/com/internousdev/login/util/DBConnector.java
@@ -8,7 +8,7 @@ public class DBConnector {
 	private static String url = "jdbc:mysql://localhost/logindb_fujie";
 
 	private static String user = "root";
-	private static String password = "mssql";
+	private static String password = "mysql";
 
 	public Connection getConnection(){
 	Connection con = null;
