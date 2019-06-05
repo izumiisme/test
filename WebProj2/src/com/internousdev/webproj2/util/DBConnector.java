0aaa3c7dea74178ba9719b74c2530e4ef89986d8
izumiisme <bmbm.taeha@gmail.com>
2019-06-05 20:25:46 +0900

    6/5

--- WebProj2/src/com/internousdev/webproj2/util/DBConnector.java
+++ WebProj2/src/com/internousdev/webproj2/util/DBConnector.java
@@ -12,7 +12,7 @@ public class DBConnector {
 	/**
 	 * データベース接続URL
 	 */
-	private static String url = "jdbc:mysql://localhost/testdb2";
+	private static String url = "jdbc:mysql://localhost/testdb1";
 	/**
 	 * データベース接続ユーザ名
 	 */
